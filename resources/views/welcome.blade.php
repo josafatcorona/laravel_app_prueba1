be4d5663d384090259100997627d5e6486f83740
JosafatCorona <josafatcoronac@gmail.com>
2019-09-12 06:28:52 -0500

    php artisan ui:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JosafatCorona
                 </div>
 
                 <div class="links">
