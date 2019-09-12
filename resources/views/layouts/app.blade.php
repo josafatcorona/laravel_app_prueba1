70431531b037fbe9d9e2dfdd7bbef18792f8c8e0
JosafatCorona <josafatcoronac@gmail.com>
2019-09-12 07:59:33 -0500

    Fonts changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/svg/logo.svg" style="max-height:25px;"></div>
-                    <div>CoroNagram</div>
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div><img src="/svg/logo.svg" style="height:20px; border-right: 1px solid #333333;" class="pr-3"></div>
+                    <div class="pl-3">CoroNagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
